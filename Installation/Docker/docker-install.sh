3af1a60e2eb56671e62e34570280daba2141940e
Leow Chun Fung <chun.fung.leow@intel.com>
2022-06-14 08:33:32 +0800

    Docker installation step update

--- Installation/Docker/docker-install.sh
+++ Installation/Docker/docker-install.sh
@@ -3,13 +3,15 @@
 echo "Official guide: https://docs.docker.com/engine/install/"
 
 sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 current_user=$USER
 sudo usermod -aG docker $current_user
