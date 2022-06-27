f19fc1056e8a3dc4b9ce87653880dc4613161c28
Leow Chun Fung <chun.fung.leow@intel.com>
2022-06-27 23:43:19 +0800

    Use fixed proxy server

--- Installation/Docker/docker-configure.sh
+++ Installation/Docker/docker-configure.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-PS3='Select the closest proxy server to this system: '
-proxys=("United States" "India" "Israel" "Ireland" "Germany" "Malaysia" "China")
-select proxy in "${proxys[@]}"
-do
-  case $proxy in
-    "United States")
-      server="proxy-us.intel.com"
-      break
-      ;;
-    "India")
-      server="proxy-iind.intel.com"
-      break
-      ;;
-    "Israel")
-      server="proxy-iil.intel.com"
-      break
-      ;;
-    "Ireland")
-      server="proxy-ir.intel.com"
-      break
-      ;;
-    "Germany")
-      server="proxy-mu.intel.com"
-      break
-      ;;
-    "Malaysia")
-      server="proxy-png.intel.com"
-      break
-      ;;
-    "China")
-      server="proxy-prc.intel.com"
-      break
-      ;;
-    *) echo "Invalid proxy";;
-  esac
-done
+#PS3='Select the closest proxy server to this system: '
+#proxys=("United States" "India" "Israel" "Ireland" "Germany" "Malaysia" "China")
+#select proxy in "${proxys[@]}"
+#do
+#  case $proxy in
+#    "United States")
+#      server="proxy-us.intel.com"
+#      break
+#      ;;
+#    "India")
+#      server="proxy-iind.intel.com"
+#      break
+#      ;;
+#    "Israel")
+#      server="proxy-iil.intel.com"
+#      break
+#      ;;
+#    "Ireland")
+#      server="proxy-ir.intel.com"
+#      break
+#      ;;
+#    "Germany")
+#      server="proxy-mu.intel.com"
+#      break
+#      ;;
+#    "Malaysia")
+#      server="proxy-png.intel.com"
+#      break
+#      ;;
+#    "China")
+#      server="proxy-prc.intel.com"
+#      break
+#      ;;
+#    *) echo "Invalid proxy";;
+#  esac
+#done
+server="proxy-iind.intel.com"
 
 echo "setup docker proxy"
 sudo systemctl stop docker
